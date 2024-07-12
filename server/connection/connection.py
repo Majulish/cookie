a6233350b245f87bce856b48934d6e5c47c62a4f
majulish <yuvalgo9@gmail.com>
2024-07-12 09:42:15 +0300

    Redid the enviroment with Next.JS, added Docker and Prisma to access and use docker.

--- server/connection/connection.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-
-mongo_host = "localhost"
-mongo_port = 27017
-mongo_db = "mydatabase"
-
-client = MongoClient(mongo_host, mongo_port)
-db = client[mongo_db]
-collection = db["mycollection"]
