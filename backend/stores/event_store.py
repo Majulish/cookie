d92f59b8abc6e22aba1eaae039d0d6be8ede097e
adiam1 <adiamshalem1@gmail.com>
2024-10-15 19:23:30 +0300

    adding event store the ability to get workers specific to event, and fixing little bugs

--- backend/stores/event_store.py
+++ backend/stores/event_store.py
@@ -1,15 +1,17 @@
-from typing import List, Optional
+from typing import List, Optional, Tuple
 import json
 
-from flask import jsonify
+from flask import jsonify, Response
 from backend.models.event import Event
+from backend.models.user import User
+from backend.models.event_workers import EventWorkers
 
 
 class EventStore:
     @staticmethod
     def create_event(new_event):
         event = Event(
-            id=new_event['id'],
+            event_id=new_event['id'],
             name=new_event['name'],
             start_time=new_event['start_time'],
             end_time=new_event['end_time'],
@@ -38,8 +40,8 @@ class EventStore:
         return Event.query.get(event_id)
 
     @staticmethod
-    def update_event(event: dict) -> Optional[Event]:
-        if not EventStore.get_event_by_id(event.id):
+    def update_event(event: Event) -> tuple[Response, int] | dict:
+        if not EventStore.get_event_by_id(event.event_id):
             return jsonify({"error": "Event not found."}), 404
 
         for key, value in event.items():
@@ -61,7 +63,28 @@ class EventStore:
     def get_all_events() -> List[Event]:
         return Event.query.all()
 
-    from typing import List, Dict
+    @staticmethod
+    def get_workers_by_event(event_id: int) -> tuple[Response, int]:
+        # Fetch the event
+        event = EventStore.get_event_by_id(event_id)
+        if not event:
+            return jsonify({"error": "Event not found."}), 404
+
+        workers = (
+            User.query
+            .join(EventWorkers, User.id == EventWorkers.user_id)
+            .filter(EventWorkers.event_id == event_id)
+            .all()
+        )
+
+        worker_data = [{
+            "id": worker.id,
+            "name": worker.name,
+            "email": worker.email,
+            "role": worker.role.value
+        } for worker in workers]
+
+        return jsonify(worker_data), 200
 
 
 
