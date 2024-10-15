6b8cb84b25e9067c799462a82389d2ed5031d351
Daniella7777 <daniellac.iisr.2004@gmail.com>
2024-10-15 22:10:51 +0300

    more fixing to the merge with main

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,9 @@
 import React from 'react';
-import { CssBaseline, ThemeProvider } from '@mui/material';
-import { createTheme } from '@mui/material/styles';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import SignIn from './pages/signin/SignIn';
 import SignUp from './pages/signup/SignUp';
 import HomePage from './pages/home/HomePage';
-
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#1976d2',
-    },
-  },
-});
 
 const queryClient = new QueryClient();
 
@@ -26,10 +16,7 @@ const router = createBrowserRouter([
 const App: React.FC = () => {
   return (
     <QueryClientProvider client={queryClient}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
         <RouterProvider router={router} />
-      </ThemeProvider>
     </QueryClientProvider>
   );
 };
