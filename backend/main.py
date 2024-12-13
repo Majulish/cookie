d85d14375fd6dba4283beb94fd094c4aa9ef6918
Daniella7777 <daniellac.iisr.2004@gmail.com>
2024-12-13 18:17:20 +0200

    corrected a change in main.py

--- backend/main.py
+++ backend/main.py
@@ -33,30 +33,7 @@ def create_app():
     app.config['JWT_REFRESH_TOKEN_EXPIRES'] = timedelta(days=30)
     jwt = JWTManager(app)
 
-    # JWT error handlers
-    @jwt.expired_token_loader
-    def expired_token_callback(jwt_header, jwt_payload):
-        return jsonify({
-            'status': 401,
-            'sub_status': 'expired',
-            'msg': 'The token has expired'
-        }), 401
-
-    @jwt.invalid_token_loader
-    def invalid_token_callback(error):
-        return jsonify({
-            'status': 401,
-            'sub_status': 'invalid',
-            'msg': 'Invalid token'
-        }), 401
-
-    @jwt.unauthorized_loader
-    def missing_token_callback(error):
-        return jsonify({
-            'status': 401,
-            'sub_status': 'missing',
-            'msg': 'No token provided'
-        }), 401
+  
 
     # SQLAlchemy configuration
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://root:example@localhost:5432/my_database'
