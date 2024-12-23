77245714ce077da639cb7aa92ea5aaf3192fbb49
majulish <yuvalgo9@gmail.com>
2024-12-23 19:44:43 +0200

    decorator addition

--- backend/stores/event_store.py
+++ backend/stores/event_store.py
@@ -45,9 +45,9 @@ class EventStore:
             raise e
 
     @staticmethod
-    def update_event(event: Dict) -> Tuple[Response, int]:
+    def update_event(event_id: int, event: Dict) -> Tuple[Response, int]:
         try:
-            existing_event = Event.find_by(id=event.get("id"))
+            existing_event = Event.find_by(id=event_id)
             if not existing_event:
                 return jsonify({"error": "Event not found"}), 404
 
