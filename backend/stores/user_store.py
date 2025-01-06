fd7c04a10a362948d76eb7e544c34d3bd0721677
majulish <yuvalgo9@gmail.com>
2025-01-06 20:34:02 +0200

    expanded the check - does the user exists looks through all unique user values

--- backend/stores/user_store.py
+++ backend/stores/user_store.py
@@ -30,6 +30,20 @@ class UserStore:
             return jsonify({"error": str(e)}), 500
 
     @staticmethod
+    def user_exists(data):
+        fields_to_check = ["username", "email", "personal_id"]
+        messages = {
+            "username": "Username already exists",
+            "email": "Email already exists",
+            "personal_id": "Personal ID already exists"
+        }
+
+        for field in fields_to_check:
+            existing_user = UserStore.find_user(field, getattr(data, field))
+            if existing_user:
+                return messages[field]
+
+    @staticmethod
     def delete_user(field: str, value: str) -> Tuple[Response, int]:
         try:
             success = User.delete_by(field, value)
