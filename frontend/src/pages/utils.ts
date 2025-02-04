581f397ca146af9868c0b802189dc587c614a64c
Daniella7777 <daniellac.iisr.2004@gmail.com>
2025-02-04 12:53:08 +0200

    oops utils was not there

---
+++ frontend/src/pages/utils.ts
@@ -0,0 +1,25 @@
+
+export const calculateAge = (dateOfBirth: string): number => {
+    const today = new Date();
+    const birthDate = new Date(dateOfBirth);
+    let age = today.getFullYear() - birthDate.getFullYear();
+    const monthDifference = today.getMonth() - birthDate.getMonth();
+  
+    if (monthDifference < 0 || (monthDifference === 0 && today.getDate() < birthDate.getDate())) {
+      age--;
+    }
+  
+    return age;
+  };
+  
+  export const isValidDate = (day: number, month: number, year: number): boolean => {
+    const date = new Date(year, month - 1, day);
+    return date.getFullYear() === year && date.getMonth() === month - 1 && date.getDate() === day;
+  };
+
+  export const combineDateTimeToISO = (date: string, time: string): string => {
+    const [day, month, year] = date.split('/').map(Number);
+    const [hours, minutes] = time.split(':').map(Number);
+    return new Date(year, month - 1, day, hours, minutes).toISOString();
+  };
+  
